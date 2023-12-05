bda6c087edbfed374c7e52f055f5fb41a6f32193
Sulu2011 <126379528+Sulu2011@users.noreply.github.com>
2023-12-05 21:38:07 +0400

    Add files via upload

---
+++ homework javascript1/script.js
@@ -0,0 +1,44 @@
+//let x=prompt("eded daxil edin")
+//let y=prompt("eded daxil edin")
+//let cavab=x*y
+//console.log(`${cavab}-dir`);
+//birinci tapsiriq
+
+//let arr=[];
+//let a=prompt("mehsul daxil edin")
+//let b=prompt("mehsul daxil edin")
+//let c=prompt("mehsul daxil edin")
+//arr.push(a)
+//arr.push(b)
+//arr.push(c)
+//console.log(arr);
+//ikici tapsiriq 
+
+//let x=prompt("eded daxil edin")
+//if(x%2==0){
+//alert("sen duz bildin")
+//}
+////else{
+//    alert("sehv bildin")
+//}
+//üçüncü tapşırıq
+
+
+//let a=prompt("meyve deyin")
+//let arr=['alma','armud','iyde','heyva','saftali','xurma']
+//let b=arr.includes(a)
+//if(b==true){
+  //  console.log("bizde bu meyve var")
+//}
+//else{
+  //  console.log("bizde bu meyve yoxdu")
+//}
+//dorduncu tapsiriq
+//let a=prompt("adinizi daxil edin")
+//let b=prompt("soyadinizi daxil edin")
+//let c=prompt("yasinizi daxil edin")
+//
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//tapsiriq 5
---
+++ homework2/script.js
@@ -0,0 +1,62 @@
+//let arr=['pomidor yumurta']
+//let arr1=['pendir corek']
+//let arr2=['stake eti']
+//let arr3=['qizildan et']
+//let arr4=['sucuq']
+//let x=prompt("ureyiniz isteyen yemeyi daxil edin");
+//switch(x){
+ /// case 'a':
+   /// console.log(arr);
+ //   break;
+ ///   case 'b':
+    //  console.log(arr1);
+     // break;
+//
+  //  case 'c':
+    //  console.log(arr2);
+      //break;
+//  case 'd':
+  //  console.log(arr3);
+    ///break;
+  //  case 'e':
+    //  console.log(arr4);
+      //break;
+     //default:
+      //  console.log('bele bir yemek yoxdu')
+//}bu 6ci tapsiriq idi 5in evvelki faylda elemisem
+//let pul=prompt("ne qeder pul qoymaq isteyirsiniz?")
+//let hesab=prompt("neceye qaliq isteyirsiz")
+//let user = {
+ //   mebleq: pul,
+   // qaliq:pul%hesab
+ // };
+ // console.log(user.mebleq);
+ // console.log(user.qaliq);
+ //7ci tapsiriq
+//let reqem=[];
+//for(let i=0;i<10;i++){
+//    reqem.push(Math.random()*(25-1)+1);
+ //   continue;
+//
+//}
+//console.log(reqem);
+//8ci tapsiriq
+
+////let a=21;
+//for(let i=0;i<3;i++){
+  //  let x=prompt("1 ile 25 arasindan eded deyin");
+    
+ ////   if(x>21){
+//        console.log("dostum bu eded cox boyukdu")
+    
+ //   }
+ //   else if(x<21){
+  //  console.log("dostum bu eded kicikdi")
+ //   }
+ //   else{
+ //      console.log("a-nı tapdin.")
+ //   }
+//}//9cu tapsiriq
+//let arrayofarray=[arr=[1,2,3],arr1=[4,5,6]];
+//console.log(arrayofarray);
+//10cu tapsiriq
